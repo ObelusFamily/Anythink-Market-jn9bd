e23fbbbfc0d9a886dc1ee2c125fd72740cbefc26
Chase Farmer <98106992+cfarmer-fearless@users.noreply.github.com>
2022-10-19 18:42:29 +0000

    revert

--- frontend/src/components/Home/MainView.js
+++ frontend/src/components/Home/MainView.js
@@ -73,7 +73,7 @@ const mapDispatchToProps = (dispatch) => ({
 
 const MainView = (props) => {
   if (props.items?.length === 0) {
-    return <NoItemsFound search={props.search} />;
+    return "";
   }
 
   return (
